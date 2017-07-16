8ad40954780665791602f112d801870a7e6fdb4a
jeffmon <jeffmon@gmail.com>
2017-07-15 19:47:21 -1000

    finished some of PUT and DELETE requests

--- app.js
+++ app.js
@@ -13,23 +13,39 @@ const stringChecker = (element) => {
   return typeof element === "string";
 };
 
+const putData = (obj) => {
+  allProducts.forEach((e) =>{
+    if(e.id === obj.body.id && parseInt(obj.params.id) === e.id){
+      e.name = obj.body.name;
+    }
+  });
+};
+
+const deleteData = (obj) => {
+  allProducts.forEach((e) => {
+    if(parseInt(obj.params.id) === e.id){
+    var location = allProducts.indexOf(e);
+    allProducts.splice(location, 1);
+    }
+  });
+};
+
 const postData = (obj) => {
   var values = Object.keys(obj).map((key) => {
     return obj[key];
   });
+
   var keys = Object.keys(obj).toString();
 
-  console.log(values);
-
-    if ( keys === 'name,price,inventory' && values.some(stringChecker) === true){
-      let price = parseInt(obj.price);
-      let inventory = parseInt(obj.inventory);
-      obj.id = count;
-      obj.price = price;
-      obj.inventory = inventory;
-      allProducts.push(obj);
-      count++;
-   }
+  if (keys === 'name,price,inventory' && values.some(stringChecker) === true) {
+    let price = parseInt(obj.price);
+    let inventory = parseInt(obj.inventory);
+    obj.id = count;
+    obj.price = price;
+    obj.inventory = inventory;
+    allProducts.push(obj);
+    count++;
+  }
 };
 
 const hbs = exphbs.create({
@@ -41,12 +57,13 @@ app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
   res.render();
 });
 
-app.post('/products', (req,res) =>{
+app.post('/products', (req, res) => {
   postData(req.body);
+  console.log("post: ");
   console.log(allProducts);
   res.json({
     "success": true
@@ -54,10 +71,26 @@ app.post('/products', (req,res) =>{
   res.end();
 });
 
+app.route("/products/:id")
+  .put((req, res) => {
+    console.log("put before: ");
+    console.log(allProducts);
+    putData(req);
+    console.log("put after: ");
+    console.log(allProducts);
+    res.end();
+  })
+  .delete((req, res) => {
+    console.log("before: ");
+    console.log(allProducts);
+    deleteData(req);
+    console.log("after: ");
+    console.log(allProducts);
+    res.end();
+  });
 
 
-const server = app.listen(PORT, () =>{
+
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
